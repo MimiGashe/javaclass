7592716a9b5bd1d32316b8967162e8f303e6dbb7
MimiGashe <136394967+MimiGashe@users.noreply.github.com>
2023-06-20 16:12:31 -0400

    H3

---
+++ comparisonandconversion.js
@@ -0,0 +1,46 @@
+//Comparison task
+5>4
+console.log(5>4);//true
+console.log("apple">"pineapple") //false
+console.log("2">"12")//true
+console.log(undefined==null)//true
+console.log(undefined===null)//false
+console.log(null=="\n0\n")/false
+console.log(null ===+"\n0\n")//false
+console.log(null==="\n0\n")//false
+
+//Conversion Task
+
+let fruits = ["Apples","Pear","Orange"];
+fruits = Number(fruits)
+console.log(typeof(fruits))
+let shoppingCart = fruits;
+let x = undefined
+x=Number(undefined)
+console.log(typeof(x))
+let y =null
+y=Number(y)
+console.log(typeof(y))
+let z =0
+z=Number(z)
+console.log(typeof(z))
+let a =1
+z=Number(a)
+console.log(typeof(a))
+let b = String
+b=Number(b)
+console.log(typeof(b))
+
+
+// Variable/type of exercise
+//let a = null;
+//console.log(typeof(a));
+//let b = undefined;
+//console.log (typeof(b));
+//let c =10n;
+//console.log (typeof(c));
+//let d = 0;
+//console.log(typeof(d));
+
+//let E = true;
+//console.log (typeof(E));
--- datatype.js
+++ datatype.js
@@ -1,19 +1,22 @@
 // Array Tasks
-let fruits = ["Apples","Pear","Orange"];
-let shoppingCart = fruits;
-shoppingCart.push("Banana");
-console.log(fruits.length);
+//let fruits = ["Apples","Pear","Orange"];
+//let shoppingCart = fruits;
+//shoppingCart.push("Banana");
+//console.log(fruits.length);
 // Variable/type of exercise
-let a = null;
-console.log(typeof(a));
-let b = undefined;
-console.log (typeof(b));
-let c =10n;
-console.log (typeof(c));
-let d = 0;
-console.log(typeof(d));
+//let a = null;
+//console.log(typeof(a));
+//let b = undefined;
+//console.log (typeof(b));
+//let c =10n;
+//console.log (typeof(c));
+//let d = 0;
+//console.log(typeof(d));
 
-let E = true;
-console.log (typeof(E));
+//let E = true;
+//console.log (typeof(E));
+x =3
+y="2"
+console.log(x-y);
 
 
