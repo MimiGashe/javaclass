6b1da16ce81101b4dc552d4cd336b87d64612ae0
MimiGashe <136394967+MimiGashe@users.noreply.github.com>
2023-06-27 17:39:03 -0400

    H5

---
+++ forHW.js
@@ -0,0 +1,24 @@
+for(let x = 1; x < 6; x++) {
+    let result= ""
+    for(let y = 1;y<=5;++y){
+    result = result + y
+}
+    console.log(result)
+};
+for(let x = 1; x < 6; x++) {
+    let result= ""
+    for(let y = 1;y<=x;y++){
+    result = result + y
+}
+    console.log(result)
+};
+console.log("done")
+console.log("done")
+for(let x = 5; x >0&& x< 6; x--) {
+    let result= ""
+    for(let y = 1;y<=x;y++){
+    result = result + x
+}
+    console.log(result)
+};
+console.log("done")
